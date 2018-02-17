ff6cf15eca5ef8f7c1d39bd0de33a21529d70869
jsotteccanic <jsotteccanic@gmail.com>
2018-02-17 18:03:13 -0500

    login terminado

--- public/src/js/constantes.js
+++ public/src/js/constantes.js
@@ -1,4 +1,4 @@
 'use strict'
 const ruta = 'http://35.182.81.135:80/';
-const local = 'http://192.168.0.12:9090/';
+const local = 'http://35.182.81.135:9090/';
 const tipoAtencion = ['Software','Hardware','Usuario'];
