bc55ed66ec7ca7a974af6f982b24d159e9d3b8f8
jsotteccanic <jsotteccanic@gmail.com>
2018-02-18 18:42:15 -0500

    terminado 1ra parte

--- public/src/js/constantes.js
+++ public/src/js/constantes.js
@@ -1,6 +1,6 @@
 'use strict'
-const servicio = 'http://35.182.81.135:80/';
-const ruta = 'http://35.182.81.135:8080/';
-// const servicio = 'http://localhost:80/';
-// const ruta = 'http://localhost:8080/';
+// const servicio = 'http://35.182.81.135:80/';
+// const ruta = 'http://35.182.81.135:8080/';
+const servicio = 'http://192.168.0.12:80/';
+const ruta = 'http://192.168.0.12:8080/';
 const tipoAtencion = ['Software','Hardware','Usuario'];
--- public/src/js/usuario.js
+++ public/src/js/usuario.js
@@ -5,10 +5,10 @@ obtenerColaboradores();
 obtenerTipoAtencion();
 window.onbeforeunload = function () {
     var rpt = confirm('Esta seguro de querer salir?');
-    if(rpt){
+    if (rpt) {
         sessionStorage.clear();
     }
-    
+
 };
 function obtenerColaboradores() {
     $.ajax({
@@ -38,30 +38,32 @@ function obtenerTipoAtencion() {
 
 }
 
-function mostrarModal() {
-    $("#modal").modal('show');
+function mostrarModalRegistro() {
+    $("#modal1").modal('show');
 }
-
+function mostrarModalPendientes() {
+    $("#modal2").modal('show');
+    consultarAtenciones();
+}
 function atender() {
     //valores = $("#formulario").form('get values');
     valores = {};
     valores.tipoAtencion = $("#tipoAtencion option:selected").val();
     valores.descripcion = $("#detalle").val();
-    valores.colaborador = $("#dni option:selected").val();   
-    
+    valores.colaborador = $("#dni option:selected").val();
+
     // valores.t_inicio = tiempo;
     console.log(valores);
     $.ajax({
-        url: servicio+'atencion/registrarAtencion',
+        url: servicio + 'atencion/registrarAtencion',
         data: valores,
         type: 'post',
         beforeSend: function (xhr) {
             xhr.setRequestHeader("Authorization", sessionStorage.getItem('sesion'));
-          },
+        },
         success: function (r) {
             if (r) {
-                $("#formulario").form('clear');
-                $("#formulario").form('reset');
+                limpiarFormulario();
                 alert('Se registro satisfactoriamente!');
             }
             else {
@@ -69,6 +71,44 @@ function atender() {
             }
         }
     });
+}
 
+function consultarAtenciones() {
+    $.ajax({
+        url: servicio + 'atencion/listarAtenciones',
+        // data: valores,
+        type: 'get',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", sessionStorage.getItem('sesion'));
+        },
+        success: function (r) {
+            $("#tbListaAtencion").empty();
+            for (i = 0; i < r.atencion.length; i++) {
+                $("#tbListaAtencion").append('<tr><td>' + r.atencion[i].colaborador.nombres + ' ' + r.atencion[i].colaborador.apellidos + '</td><td>' + r.atencion[i].colaborador.area + '</td><td>' + r.atencion[0].fecha_inicio + '</td><td><button class="ui inverted red basic button" onclick="finalizarAtencion(\'' + r.atencion[i]._id + '\')" >Cerrar</button></td></tr>');
+            }
+        }
+    });
+}
 
+function finalizarAtencion(id) {
+    $.ajax({
+        url: servicio + 'atencion/cerrarAtencion/' + id,
+        type: 'put',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", sessionStorage.getItem('sesion'));
+        },
+        success: function (r) {
+            debugger;
+            $("#tbListaAtencion").empty();
+            consultarAtenciones();
+        }
+    });
+}
+function cancelarRegistro() {
+    limpiarFormulario();
+}
+function limpiarFormulario() {
+    $("#formulario").form('clear');
+    $("#formulario").form('reset');
+    $("#tbListaAtencion").empty();
 }
